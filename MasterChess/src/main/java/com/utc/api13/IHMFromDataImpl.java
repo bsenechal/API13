962892c930dace74ad9eb6f53f693e3a608b083b
Durand-Henriot Clarisse <clarisse.durand@etu.utc.fr>
2015-11-12 18:04:36 +0100

    push ihm 2

---
+++ MasterChess/src/main/java/com/utc/api13/IHMFromDataImpl.java
@@ -0,0 +1,114 @@
+package com.utc.api13; 
+
+import com.utc.api13.IIHMFromData;
+
+public class IHMFromDataImpl implements IIHMFromData {
+	
+	public IHMFromDataImpl() {
+		
+	}
+	
+	public void didReceiveBoard() {
+		
+	}
+	
+	public void didReceiveEndOfGameBySurrender() {
+		
+	}
+	
+	public void didReceiveEndOfGameByVictory() {
+		
+	}
+	
+	public void didReceiveEndOfGameByLoss() {
+		
+	}
+	
+	public void didReceiveEndOfGameByTie() {
+		
+	}
+	
+	public void didReceiveEndOfObservation() {
+		
+	}
+	public void didReceiveNewChatMessage() {
+		
+	}
+	
+	public void answerForLeavingIsYes() {
+		
+	}
+	
+	public void answerForLeavingIsNo() {
+		
+	}
+	
+	public void didReceiveRequestToInterruptGame() {
+		
+	}
+	
+	public void publicProfileHasBeenCorrectlyUpdated() {
+		
+	}
+	
+	public void publicProfileHasFailedToBeUpdated() {
+		
+	}
+	
+	public void didConnectSuccessfully() {
+		
+	}
+	
+	public void didRefuseConnectionToServer() {
+		
+	}
+	
+	public void listOfObserversHasBeenUpdated() {
+		
+	}
+	
+	public void didReceiveListOfConnectedUsers() {
+		
+	}
+	
+	public void moveWasAccepted() {
+		
+	}
+	
+	public void moveWasDenied() {
+		
+	}
+	
+	public void newMoveFromOpponent() {
+		
+	}
+	
+	public void didReceiveProfileOtherUser() {
+		
+	}
+	
+	public void errorReceivingProfileOtherUser() {
+		
+	}
+	
+	public void didReceivePropositionOfGame() {
+		
+	}
+	
+	public void propositionOfGameAccepted() {
+		
+	}
+	
+	public void propositionOfGameDenied() {
+		
+	}
+	
+	public void didReceiveListOfOngoingAndReplayedGames() {
+		
+	}
+	
+	public void didReceiveErrorInChatMessage() {
+		
+	}
+	
+}
