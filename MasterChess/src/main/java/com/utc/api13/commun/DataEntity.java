07d48d12425485246fc3e5c1e5ce2a8f52bb47da
Meyer Ulysse <ulysse.meyer@etu.utc.fr>
2015-11-12 18:56:22 +0100

    modif DataEntity

--- MasterChess/src/main/java/com/utc/api13/commun/DataEntity.java
+++ MasterChess/src/main/java/com/utc/api13/commun/DataEntity.java
@@ -1,8 +1,25 @@
 package com.utc.api13.commun;
 
-public interface DataEntity<T> {
-    void setId(final T id);
+import java.io.Serializable;
+import java.util.UUID;
 
-    T getId();
+/**
+ * Interface générique des entities
+ * @author Amstrong
+ *
+ */
+public interface DataEntity extends Serializable {
+	
+	/**
+	 * Permet de récupérer l'identifiant de l'entity
+	 * @return l'identifiant de l'entity
+	 */
+	UUID getId();
+	
+	/**
+	 * Permet de setter l'identifiant de l'entity
+	 * @param id identifiant de l'entity
+	 */
+	void setId(UUID id);
 
 }
