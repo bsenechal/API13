431b0ade6e7ccd0a9902b656dd7e38277733fc56
Benoit Senechal <benoit.senechal@etu.utc.fr>
2015-11-12 22:09:09 +0100

    Déplacement d'une énum

--- MasterChess/src/main/java/com/utc/api13/commun/entities/PieceColorEnum.java
+++
@@ -1,6 +0,0 @@
-package com.utc.api13.commun.entities;
-
-public enum PieceColorEnum {
-    WHITE, BLACK
-
-}
---
+++ MasterChess/src/main/java/com/utc/api13/commun/enumerations/PieceColorEnum.java
@@ -0,0 +1,6 @@
+package com.utc.api13.commun.enumerations;
+
+public enum PieceColorEnum {
+    WHITE, BLACK
+
+}
