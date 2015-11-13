f9881bad049e069381a079461d048360a5cd8bdd
Durand-Henriot Clarisse <clarisse.durand@etu.utc.fr>
2015-11-13 16:45:38 +0100

    file IHM welcome page controller : name changed

---
+++ MasterChess/src/main/java/com/utc/api13/client/ihm/IHMWelcomePageController.java
@@ -0,0 +1,74 @@
+package com.utc.api13.client.ihm; 
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Text;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollBar;
+import javafx.scene.control.SplitPane;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.ToolBar;
+
+public class IHMWelcomePageController {
+	
+	//+ tous les listeners
+	ClientToIHMImpl clientToIHM ; 
+	@FXML
+	BorderPane mainBorderPane; 
+	@FXML
+	SplitPane splitPane, centralSplitPane, rightSplitPane, splitPane3; 
+	@FXML
+	AnchorPane anchorPane, rightAnchorPane, leftAnchorPane, bottomLeftAnchorPane, topLeftAnchorPane; 
+	@FXML
+	ImageView iconHelp, iconParam, iconProfile, iconNotif; 
+	@FXML
+	Label title, currentGamesLabel, savedGamesLabel, connectedUserLabel; 
+	@FXML
+	Text userLabel; 
+	@FXML
+	TableView currentGamesTable, savedGamesTable, connectedUserTable; 
+	@FXML
+	TableColumn currentGamesId, currentGamesPlayer1, currentGamesPlayer2, currentGamesTime, currentGamesObs;
+	@FXML
+	TableColumn savedGamesId, savedGamesPlayer1, savedGamesPlayer2, savedGamesDate, savedGamesReplay;
+	@FXML
+	TableColumn connectedUserLogin, connectedUserStatus, connectedUserStat, connectedUserActionIcon;
+	@FXML
+	ScrollBar currentGamesScrollbar, savedGamesScrollbar, connectedUserScrollbar; 
+	
+	public IHMWelcomePageController() { 
+		clientToIHM = new ClientToIHMImpl(); 
+		//+ listeners
+	}
+	
+	public void initialize() {
+		//bindings
+	}
+	
+	public void onHelpClicked() {
+
+	}
+	
+	public void onParamClicked() {
+		
+	}
+	
+	public void onNotifClicked() {
+		
+	}
+	
+	public void setListConnectedUser() {
+		
+	}
+	
+	public void setListCurrentGames() {
+		
+	}
+	
+	public void setListSavedGames() {
+		
+	}
+}
