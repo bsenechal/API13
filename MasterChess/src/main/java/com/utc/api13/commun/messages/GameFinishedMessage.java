dccad85b61073b5c75d48f26653f1e4bc50b4347
Xiaowei ZHANG <phycctv@gmail.com>
2015-11-18 00:39:41 +0100

    add new messages

---
+++ MasterChess/src/main/java/com/utc/api13/commun/messages/GameFinishedMessage.java
@@ -0,0 +1,53 @@
+package com.utc.api13.commun.messages;
+
+import java.util.UUID;
+
+import org.apache.log4j.Logger;
+
+import com.utc.api13.commun.entities.GameEntity;
+import com.utc.api13.commun.entities.MoveEntity;
+import com.utc.api13.server.com.ServerHanlder;
+
+import io.netty.channel.ChannelHandlerContext;
+
+public class GameFinishedMessage extends Message {
+	private static final Logger logger = Logger.getLogger(GameFinishedMessage.class);
+	GameEntity game;
+
+	/**
+	 * @param sender
+	 * @param receiver
+	 * @param game
+	 */
+	public GameFinishedMessage(UUID sender, UUID receiver, GameEntity game) {
+		super(sender, receiver);
+		this.game = game;
+	}
+
+	@Override
+	public void proceed(ChannelHandlerContext ctx) {
+		// TODO Auto-generated method stub
+
+	}
+
+
+	public GameEntity getGame() {
+		return game;
+	}
+
+	public void setGame(GameEntity game) {
+		this.game = game;
+	}
+
+	@Override
+	public void proceedServer(ChannelHandlerContext ctx) {
+		try {
+			ServerHanlder.getInstance().replyAll(ctx,new GameFinishedMessage(new UUID(0, 0), new UUID(0, 0), game ));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
