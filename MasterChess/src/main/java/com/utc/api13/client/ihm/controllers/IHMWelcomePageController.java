58c9442f5c6ad0830601aa8de0eedd52a49a83ef
ptroyard <pierre.troyard@gmail.com>
2015-11-24 21:01:40 +0100

    [IHM] Listener implementation / Binding tableView / Creation of setBindings() / Bouchon (instead of waiting the method of DATA)

--- MasterChess/src/main/java/com/utc/api13/client/ihm/controllers/IHMWelcomePageController.java
+++ MasterChess/src/main/java/com/utc/api13/client/ihm/controllers/IHMWelcomePageController.java
@@ -1,4 +1,4 @@
-package com.utc.api13.client.ihm.controllers; 
+package com.utc.api13.client.ihm.controllers;
 
 import java.io.IOException;
 
@@ -12,6 +12,8 @@ import com.utc.api13.commun.entities.AUserEntity;
 import com.utc.api13.commun.entities.GameEntity;
 import com.utc.api13.commun.entities.PublicUserEntity;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.collections.ObservableSet;
 import javafx.collections.SetChangeListener;
@@ -33,29 +35,30 @@ import javafx.scene.control.SplitMenuButton;
 import javafx.scene.control.SplitPane;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class IHMWelcomePageController {
-	private IHMManager IHMManager; 
+	private IHMManager IHMManager;
 	private AppClient mainApp;
-	private IClientToIHM myIClientToIHM; 
-	
+	private IClientToIHM myIClientToIHM;
+
 	@FXML
-	BorderPane mainBorderPane; 
+	BorderPane mainBorderPane;
 	@FXML
-	SplitPane splitPane, centralSplitPane, rightSplitPane, splitPane3; 
+	SplitPane splitPane, centralSplitPane, rightSplitPane, splitPane3;
 	@FXML
-	AnchorPane anchorPane, rightAnchorPane, leftAnchorPane, bottomLeftAnchorPane, topLeftAnchorPane; 
+	AnchorPane anchorPane, rightAnchorPane, leftAnchorPane, bottomLeftAnchorPane, topLeftAnchorPane;
 	@FXML
-	ImageView iconHelp, iconParam, iconProfile, iconNotif, infoTest; 
+	ImageView iconHelp, iconParam, iconProfile, iconNotif, infoTest;
 	@FXML
-	Label title, currentGamesLabel, savedGamesLabel, connectedUserLabel; 
+	Label title, currentGamesLabel, savedGamesLabel, connectedUserLabel;
 	@FXML
-	Text userLabel; 
+	Text userLabel;
 	@FXML
-	TableView currentGamesTable, savedGamesTable, connectedUserTable; 
+	TableView currentGamesTable, savedGamesTable, connectedUserTable;
 	@FXML
 	TableColumn currentGamesId, currentGamesPlayer1, currentGamesPlayer2, currentGamesTime, currentGamesObs;
 	@FXML
@@ -63,122 +66,148 @@ public class IHMWelcomePageController {
 	@FXML
 	TableColumn connectedUserLogin, connectedUserStatus, connectedUserStat, connectedUserActionIcon;
 	@FXML
-	SplitMenuButton paramSplitMenuButton; 
+	SplitMenuButton paramSplitMenuButton;
 	@FXML
-	ScrollBar currentGamesScrollbar, savedGamesScrollbar, connectedUserScrollbar; 
+	ScrollBar currentGamesScrollbar, savedGamesScrollbar, connectedUserScrollbar;
+
 	@FXML
 	private void onHelpClicked(Event event) {
 	}
+
 	@FXML
 	private void onParamClicked(Event event) {
 	}
+
 	@FXML
 	private void onNotifClicked(Event event) {
 	}
+
 	@FXML
-	public void onModifyProfileClicked() { 
+	public void onModifyProfileClicked() {
 	}
+
 	@FXML
 	public void onLogOutClicked() {
-		myIClientToIHM.disconnect();  
+		myIClientToIHM.disconnect();
 	}
+
 	@FXML
 	public void onSettingsClicked() {
 	}
+
 	@FXML
-	public void onUserInfoClicked() throws IOException  {
-		Stage stage; 
+	public void onUserInfoClicked() throws IOException {
+		Stage stage;
 		Parent root;
 		stage = new Stage();
 		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/userInfoPopUp.fxml"));
 		root = (Pane) fxmlLoader.load();
 		UserInfoPopUpController controller = fxmlLoader.getController();
-        controller.setMainApp(this.mainApp);
+		controller.setMainApp(this.mainApp);
 		stage.setScene(new Scene(root));
 		stage.setTitle("User Information");
-		stage.initModality(Modality.APPLICATION_MODAL); 
+		stage.initModality(Modality.APPLICATION_MODAL);
 		stage.showAndWait();
 	}
-	
+
 	@FXML
-	public void onMyInfoClicked() throws IOException  {
-		Stage stage; 
+	public void onMyInfoClicked() throws IOException {
+		Stage stage;
 		Parent root;
 		stage = new Stage();
 		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/myInfoPopUp.fxml"));
 		root = (Pane) fxmlLoader.load();
 		MyInfoPopUpController controller = fxmlLoader.getController();
-        controller.setMainApp(this.mainApp);
+		controller.setMainApp(this.mainApp);
 		stage.setScene(new Scene(root));
 		stage.setTitle("My Information");
-		stage.initModality(Modality.APPLICATION_MODAL); 
+		stage.initModality(Modality.APPLICATION_MODAL);
 		stage.showAndWait();
 	}
-	
-	public IHMWelcomePageController() { 
-		initialize(); 
+
+	public IHMWelcomePageController() {
 	}
-	
-	public void initialize() {
-	}
-	
+
 	public void setMainApp(AppClient app) {
-		this.mainApp=app; 
-		//this.currentGamesLabel.setText(""); 
-        //initialiser avec login de l'user connecté : ATTENTE DATA 
+		this.mainApp = app;
+		// this.currentGamesLabel.setText("");
+		// initialiser avec login de l'user connecté : ATTENTE DATA
 	}
-	
-	public void setListConnectedUser() {
-		
-	}
-	
-	public void setListCurrentGames() {
-		
-	}
-	
-	public void setListSavedGames() {
-		
-	}
-	
-	public void setControllerContext(IHMManager ihmManager){
+
+	private ObservableList<AUserEntity> observableListConnectedUser = FXCollections.observableArrayList(); 
+
+	public void setControllerContext(IHMManager ihmManager) {
 		this.IHMManager = ihmManager;
-		if(ihmManager!=null) this.myIClientToIHM=IHMManager.getClientToIHM(); 
+		if (ihmManager != null)
+			this.myIClientToIHM = IHMManager.getClientToIHM();
 		setListenersOnLoad();
+		setBindingsOnLoad();
 	}
-	
-	public void setListenersOnLoad()
-	{
+
+	public void setListenersOnLoad() {
 		// Demande de la liste des users
-		//-------------------------------
-		SetChangeListener<AUserEntity> connectedUserListListener;
-		connectedUserListListener = change -> 
-		{
-			/*System.out.println("OK LISTENER Users");
-			bindings => setListConnectedUser(list); + implémenter méthode*/
-		};
-		//myIClientToIHM.getUserList().addListener(connectedUserListListener);
-		
+		// -------------------------------
+		observableListConnectedUser.addListener
+		(
+				new ListChangeListener<AUserEntity>() 
+				{
+					@Override
+					public void onChanged(javafx.collections.ListChangeListener.Change<? extends AUserEntity> c) 
+					{
+						connectedUserTable.setItems(observableListConnectedUser);
+					}
+				}
+		 );
+
+		setBouchon(); // a retirer lorsque data OK et remplacer l'objet du listener
 		
 		// Demande de la liste des jeux
-		//-------------------------------
-		SetChangeListener<GameEntity> currentGamesListListener;
-		currentGamesListListener = change -> 
-		{
-			/*System.out.println("OK LISTENER Game");
-			bindings => setListCurrentGames(list); + implémenter méthode*/
-		};
-		//myIClientToIHM.getAllGames().addListener(currentGamesListListener);
-		
-		
+		// -------------------------------
+		/*SetChangeListener<GameEntity> currentGamesListListener;
+		currentGamesListListener = change -> {
+			
+			 * System.out.println("OK LISTENER Game"); bindings =>
+			 * setListCurrentGames(list); + implémenter méthode
+			 
+		};*/
+		// myIClientToIHM.getAllGames().addListener(currentGamesListListener);
+
 		// Demande de la liste des parties sauvegardées
-		//-------------------------------
-		SetChangeListener<GameEntity> savedGamesListListener;
-		savedGamesListListener = change -> 
-		{
-			/*System.out.println("OK LISTENER Saved Game");
-			bindings => setListSavedGames(list); + implémenter méthode*/
-		};
-		//myIClientToIHM.getSavedGames().addListener(savedGamesListListener);
-		
+		// -------------------------------
+		/*SetChangeListener<GameEntity> savedGamesListListener;
+		savedGamesListListener = change -> {
+			
+			 * System.out.println("OK LISTENER Saved Game"); bindings =>
+			 * setListSavedGames(list); + implémenter méthode
+			 
+		};*/
+		// myIClientToIHM.getSavedGames().addListener(savedGamesListListener);
+
+	}
+	
+	public void setBindingsOnLoad() 
+	{
+		//liste des users
+		//---------------
+		connectedUserLogin.setCellValueFactory(new PropertyValueFactory<AUserEntity, String>("Login"));
+		connectedUserStatus.setCellValueFactory(new PropertyValueFactory<AUserEntity, String>("Status"));
+		connectedUserStat.setCellValueFactory(new PropertyValueFactory<AUserEntity, String>("NbWon"));
+	}
+	
+	public void setBouchon() {
+		/*bouchon => ajouter setListConnectedUser() sur un listener de bouton pour test temporaire*/
+		AUserEntity userEntity1 = new PublicUserEntity();
+		AUserEntity userEntity2= new PublicUserEntity();
+		userEntity1.setLogin("login1");
+		userEntity1.setStatus(true);
+		userEntity1.setNbPlayed(20);
+        userEntity1.setNbWon(12);
+		userEntity2.setLogin("login2");
+		userEntity2.setStatus(false);
+		userEntity2.setNbPlayed(267);
+        userEntity2.setNbWon(123);
+        observableListConnectedUser.add(userEntity1);
+        observableListConnectedUser.add(userEntity2);
+		/*END Bouchon*/
 	}
 }
