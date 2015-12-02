e3bd3d4e6a049c276c36dfef84f6beb0c2ce3664
amsonko <93asonko@gmail.com>
2015-12-02 14:47:36 +0100

    catch d'exception

--- MasterChess/src/test/java/com/utc/api13/client/data/ClientDataToIHMImplTest.java
+++ MasterChess/src/test/java/com/utc/api13/client/data/ClientDataToIHMImplTest.java
@@ -11,6 +11,8 @@ import org.junit.Test;
 import com.utc.api13.client.data.entities.PrivateUserEntity;
 import com.utc.api13.commun.entities.GameEntity;
 import com.utc.api13.commun.entities.PublicUserEntity;
+import com.utc.api13.commun.exceptions.FunctionalException;
+import com.utc.api13.commun.exceptions.TechnicalException;
 
 /**
  * @author DATA
@@ -38,7 +40,12 @@ public class ClientDataToIHMImplTest {
 	public void disconnectTest() {
 		dataClientManager.setUserLocal(new PrivateUserEntity());
 
-		dataClientManager.getClientDataToIHMImpl().disconnect();
+		try {
+			dataClientManager.getClientDataToIHMImpl().disconnect();
+		} catch (TechnicalException | FunctionalException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 		Assert.assertNotNull("dataClientManager shouldn't be null", dataClientManager);
 		Assert.assertNull("UserLocal should be null", dataClientManager.getUserLocal());
