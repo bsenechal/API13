9e2ba1470ef0024487f2850479dda131d1fe6615
Benoit Senechal <benoit.senechal@etu.utc.fr>
2016-01-19 13:36:16 +0100

    [DATA] Correction indentation

--- MasterChess/src/test/java/com/utc/api13/commun/entities/pieces/KnightEntityTest.java
+++ MasterChess/src/test/java/com/utc/api13/commun/entities/pieces/KnightEntityTest.java
@@ -24,7 +24,7 @@ public class KnightEntityTest {
     private GameEntity game;
     private PublicUserEntity blackPlayer;
     private PublicUserEntity whitePlayer;
-    
+
     /**
      * @throws java.lang.Exception
      */
@@ -47,32 +47,39 @@ public class KnightEntityTest {
 
     @Test
     public void generateAvailableMovesTest() {
-        KnightEntity whiteKnight = (KnightEntity) game.getPieceFromPosition(new PositionEntity(2,1));
-        
+        KnightEntity whiteKnight = (KnightEntity) game.getPieceFromPosition(new PositionEntity(2, 1));
+
         // Test des déplacements à l'init du jeu (2 déplacements possibles)
         List<PositionEntity> availablesPositionsWhiteKnight = whiteKnight.generateAvailableMoves(game);
-        Assert.assertFalse("availablesPositionsWhiteKnight shouldn't be empty", availablesPositionsWhiteKnight.isEmpty());
-        Assert.assertEquals("availablesPositionsWhiteKnight should be equal to 2", availablesPositionsWhiteKnight.size(), 2);
-        
+        Assert.assertFalse("availablesPositionsWhiteKnight shouldn't be empty",
+                availablesPositionsWhiteKnight.isEmpty());
+        Assert.assertEquals("availablesPositionsWhiteKnight should be equal to 2",
+                availablesPositionsWhiteKnight.size(), 2);
+
         // Ajout d'un pion de l'autre couleur pour vérifier s'il peut le prendre
         PawnEntity blackPawn = new PawnEntity(PieceColorEnum.BLACK, 1);
-        blackPawn.setPosition(new PositionEntity(1,3));
-        
+        blackPawn.setPosition(new PositionEntity(1, 3));
+
         game.getBlackPieces().add(blackPawn);
-        
+
         availablesPositionsWhiteKnight = whiteKnight.generateAvailableMoves(game);
-        Assert.assertFalse("availablesPositionsWhiteKnight shouldn't be empty", availablesPositionsWhiteKnight.isEmpty());
-        Assert.assertEquals("availablesPositionsWhiteKnight should be equal to 2", availablesPositionsWhiteKnight.size(), 2);
-        
-        // Ajout d'un pion de la même couleur pour vérifier que le déplacement n'est plus disponible
+        Assert.assertFalse("availablesPositionsWhiteKnight shouldn't be empty",
+                availablesPositionsWhiteKnight.isEmpty());
+        Assert.assertEquals("availablesPositionsWhiteKnight should be equal to 2",
+                availablesPositionsWhiteKnight.size(), 2);
+
+        // Ajout d'un pion de la même couleur pour vérifier que le déplacement
+        // n'est plus disponible
         PawnEntity whitePawn = new PawnEntity(PieceColorEnum.WHITE, 1);
-        whitePawn.setPosition(new PositionEntity(3,3));
-        
+        whitePawn.setPosition(new PositionEntity(3, 3));
+
         game.getWhitePieces().add(whitePawn);
-        
+
         availablesPositionsWhiteKnight = whiteKnight.generateAvailableMoves(game);
-        Assert.assertFalse("availablesPositionsWhiteKnight shouldn't be empty", availablesPositionsWhiteKnight.isEmpty());
-        Assert.assertEquals("availablesPositionsWhiteKnight should be equal to 1", availablesPositionsWhiteKnight.size(), 1);
+        Assert.assertFalse("availablesPositionsWhiteKnight shouldn't be empty",
+                availablesPositionsWhiteKnight.isEmpty());
+        Assert.assertEquals("availablesPositionsWhiteKnight should be equal to 1",
+                availablesPositionsWhiteKnight.size(), 1);
     }
 
 }
