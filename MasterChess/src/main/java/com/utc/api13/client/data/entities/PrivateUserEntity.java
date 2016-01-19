c1977955316fd27db8671eb29e361a4ae5290a12
lucie-vannier <lucie.vannier@etu.utc.fr>
2016-01-19 12:56:09 +0100

    Ajout des para de connection dans le private user

--- MasterChess/src/main/java/com/utc/api13/client/data/entities/PrivateUserEntity.java
+++ MasterChess/src/main/java/com/utc/api13/client/data/entities/PrivateUserEntity.java
@@ -1,20 +1,38 @@
 package com.utc.api13.client.data.entities;
 
 import java.util.List;
-import java.util.Map;
 
 import com.utc.api13.commun.entities.AUserEntity;
 import com.utc.api13.commun.entities.GameEntity;
 
 public class PrivateUserEntity extends AUserEntity {
-
+    
     private static final long serialVersionUID = -5408789931031693916L;
     private String password;
     private String imagePath;
     // List of saved games by the local user
     private List<GameEntity> savedGames;
     private GameEntity game;
-    private Map<String, Integer> connections;
+    
+    public class Conn {
+        String port; 
+        String serveur; 
+        
+        public Conn(String serv, String port){
+            this.serveur=serv; 
+            this.port= port; 
+        }
+
+        public String getPort() {
+            return port;
+        }
+
+        public void setPort(String port) {
+            this.port = port;
+        }
+   
+    }
+    private List<Conn> connections;
 
     // TODO Lors du merge de l'integ et donc de l'ajout du constructeur ajouter
     // la ligne suivante
@@ -93,18 +111,32 @@ public class PrivateUserEntity extends AUserEntity {
     }
 
     /**
-     * @return the connections map (serveur and port)
+     * @return the connections list (serveur and port)
      */
-    public Map<String, Integer> getConnections() {
+    public List<Conn> getConnections() {
         return connections;
     }
 
     /**
-     * @param Map<IP
-     *            serveur, port serveur> the used connections by the user
+     * @param List<Conn> the used connections by the user
      */
-    public void setConnections(Map<String, Integer> connections) {
+    public void setConnections(List<Conn> connections) {
         this.connections = connections;
     }
-
+    
+    /**
+     * @return the last connection by the user
+     */
+    
+    public Conn getLastConnection(){
+        return connections.get(connections.size());
+    }
+    
+    /**
+     * @param String serveur   the used serveur by the user
+     * @param String port   the used port by the user
+     */
+    public void addConnection(String serveur, String port){
+        connections.add(new Conn(serveur, port));
+    }
 }
