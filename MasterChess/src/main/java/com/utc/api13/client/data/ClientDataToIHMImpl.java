fb062ecf7a7ecafe1e0d16023e68c17103f56d67
lucie-vannier <lucie.vannier@etu.utc.fr>
2016-01-20 14:48:03 +0100

    Implementation surrender dans ClientDataToIHM

--- MasterChess/src/main/java/com/utc/api13/client/data/ClientDataToIHMImpl.java
+++ MasterChess/src/main/java/com/utc/api13/client/data/ClientDataToIHMImpl.java
@@ -264,6 +264,9 @@ public class ClientDataToIHMImpl implements IClientDataToIHM {
     @Override
     public void surrender() {
         // TODO Auto-generated method stub
+        Assert.notNull(dataClientManager.getUserLocal(),
+                "[ClientDataToIHMImpl][requestPlayerForLeaving] UserLocal shouldn't be null");
+        dataClientManager.getIClientComToData().surrender(dataClientManager.getUserLocal().getId());
 
     }
 
