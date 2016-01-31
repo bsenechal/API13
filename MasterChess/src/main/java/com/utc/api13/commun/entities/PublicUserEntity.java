75ec9f64d2649feee79ce02185207a2cd8d94b9b
Xiaowei ZHANG <phycctv@gmail.com>
2016-01-31 15:40:46 +0100

    correction bug

--- MasterChess/src/main/java/com/utc/api13/commun/entities/PublicUserEntity.java
+++ MasterChess/src/main/java/com/utc/api13/commun/entities/PublicUserEntity.java
@@ -43,23 +43,24 @@ public class PublicUserEntity extends AUserEntity {
         setNbPlayed(privateUser.getNbPlayed());
         setNbWon(privateUser.getNbWon());
         // extract bytes from image
-        // extract bytes from image
         BufferedImage image;
-        try {
-            image = ImageIO.read(new File(privateUser.getImagePath()));
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            try {
-                ImageIO.write(image, "png", baos);
-
-                byte[] res=baos.toByteArray();
-                setImage(res);
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        } catch (IOException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
+        if(privateUser.getImagePath()!=null){
+	        try {
+	            image = ImageIO.read(new File(privateUser.getImagePath()));
+	            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+	            try {
+	                ImageIO.write(image, "png", baos);
+	
+	                byte[] res=baos.toByteArray();
+	                setImage(res);
+	            } catch (IOException e) {
+	                // TODO Auto-generated catch block
+	                e.printStackTrace();
+	            }
+	        } catch (IOException e1) {
+	            // TODO Auto-generated catch block
+	            e1.printStackTrace();
+	        }
         }
         setAllowedToChat(false);
     }
