7270fbdb24d2787f4acf614b309410071d87da80
Xiaowei ZHANG <phycctv@gmail.com>
2016-01-31 03:37:32 +0100

    correction

--- MasterChess/src/main/java/com/utc/api13/server/data/DataServerManager.java
+++ MasterChess/src/main/java/com/utc/api13/server/data/DataServerManager.java
@@ -16,71 +16,73 @@ import com.utc.api13.commun.entities.PublicUserEntity;
  */
 public class DataServerManager {
 
-	// Interface DATA
-	private ServerDataToComImpl serverDataToComImpl;
-	private List<PublicUserEntity> currentUsers;
-	private List<GameEntity> currentGames;
+    // Interface DATA
+    private ServerDataToComImpl serverDataToComImpl;
+    private List<PublicUserEntity> currentUsers;
+    private List<GameEntity> currentGames;
 
-	public DataServerManager() {
-		super();
-		this.serverDataToComImpl = new ServerDataToComImpl(this);
-		this.currentGames = new ArrayList<GameEntity>();
-		this.currentUsers = new ArrayList<PublicUserEntity>();
-	}
+    public DataServerManager() {
+        super();
+        this.serverDataToComImpl = new ServerDataToComImpl(this);
+        this.currentGames = new ArrayList<GameEntity>();
+        this.currentUsers = new ArrayList<PublicUserEntity>();
+    }
 
-	/**
-	 * @return the currentUsers
-	 */
-	public List<PublicUserEntity> getCurrentUsers() {
-		return currentUsers;
-	}
 
-	/**
-	 * @param currentUsers
-	 *            the currentUsers to set
-	 */
-	public void setCurrentUsers(final List<PublicUserEntity> currentUsers) {
-		this.currentUsers = currentUsers;
-	}
 
-	/**
-	 * @return the currentGames
-	 */
-	public List<GameEntity> getCurrentGames() {
-		return currentGames;
-	}
+    /**
+     * @return the currentUsers
+     */
+    public List<PublicUserEntity> getCurrentUsers() {
+        return currentUsers;
+    }
 
-	/**
-	 * @param currentGames
-	 *            the currentGames to set
-	 */
-	public void setCurrentGames(final List<GameEntity> currentGames) {
-		this.currentGames = currentGames;
-	}
+    /**
+     * @param currentUsers
+     *            the currentUsers to set
+     */
+    public void setCurrentUsers(final List<PublicUserEntity> currentUsers) {
+        this.currentUsers = currentUsers;
+    }
 
-	/**
-	 * @return the serverToCommImpl
-	 */
-	public ServerDataToComImpl getServerDataToComImpl() {
-		return serverDataToComImpl;
-	}
+    /**
+     * @return the currentGames
+     */
+    public List<GameEntity> getCurrentGames() {
+        return currentGames;
+    }
 
-	/**
-	 * @param serverDataToComImpl
-	 *            the serverToCommImpl to set
-	 */
-	public void setServerDataToComImpl(ServerDataToComImpl serverDataToComImpl) {
-		this.serverDataToComImpl = serverDataToComImpl;
-	}
+    /**
+     * @param currentGames
+     *            the currentGames to set
+     */
+    public void setCurrentGames(final List<GameEntity> currentGames) {
+        this.currentGames = currentGames;
+    }
 
-	/**
-	 * Return Game By ID
-	 * 
-	 * @author ulyss_000
-	 * @param gameID
-	 * @return
-	 */
-	public GameEntity getGameById(UUID gameID) {
-		return this.getCurrentGames().stream().filter(g -> gameID.equals(g.getId())).findFirst().orElse(null);
-	}
+    /**
+     * @return the serverToCommImpl
+     */
+    public ServerDataToComImpl getServerDataToComImpl() {
+        return serverDataToComImpl;
+    }
+
+    /**
+     * @param serverDataToComImpl
+     *            the serverToCommImpl to set
+     */
+    public void setServerDataToComImpl(ServerDataToComImpl serverDataToComImpl) {
+        this.serverDataToComImpl = serverDataToComImpl;
+    }
+
+    /**
+     * Return Game By ID
+     * 
+     * @author ulyss_000
+     * @param gameID
+     * @return
+     */
+    public GameEntity getGameById(UUID gameID) {
+        return this.getCurrentGames().stream().filter(g -> gameID.equals(g.getId())).findFirst().orElse(null);
+    }
 }
